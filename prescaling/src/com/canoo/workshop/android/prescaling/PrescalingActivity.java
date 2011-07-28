27989aefd92e5d6d3b2676f53cfe693279b0d37b
Andreas Hölzl <andreas.hoelzl@canoo.com>
2011-07-28 08:56:23 +0200

    upscaling measure

--- prescaling/src/com/canoo/workshop/android/prescaling/PrescalingActivity.java
+++ prescaling/src/com/canoo/workshop/android/prescaling/PrescalingActivity.java
@@ -2,6 +2,12 @@ package com.canoo.workshop.android.prescaling;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PrescalingActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,17 @@ public class PrescalingActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final ImageView imageView = (ImageView) findViewById(R.id.icon);
+        final TextView textView = (TextView) findViewById(R.id.text);
+        
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+		        String text = imageView.getWidth()+", "+imageView.getHeight()+" | "+imageView.getDrawable().getBounds();
+				textView.setText(text);
+				Log.i("measure", text);
+			}
+		});
+        
     }
 }
