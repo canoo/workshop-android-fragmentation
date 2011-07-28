697fc1a62eb391e1dd9bc6cf2338cd534342cc9b
Andreas Hölzl <andreas.hoelzl@canoo.com>
2011-07-28 07:38:23 +0200

    initial commit

---
+++ prescaling/src/com/canoo/workshop/android/prescaling/PrescalingActivity.java
@@ -0,0 +1,13 @@
+package com.canoo.workshop.android.prescaling;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PrescalingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
