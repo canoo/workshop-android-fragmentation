f60b4fc3d0b0c8cdbe80dabefcf231072255ca8e
Andreas Hölzl <andreas.hoelzl@canoo.com>
2011-08-02 09:11:36 +0200

    basic resizing drawable implementation

--- scalable_bg_image/src/com/canoo/workshop/android/bgimage/DrawableBackgroundImageActivity.java
+++ scalable_bg_image/src/com/canoo/workshop/android/bgimage/DrawableBackgroundImageActivity.java
@@ -1,6 +1,9 @@
 package com.canoo.workshop.android.bgimage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 
 public class DrawableBackgroundImageActivity extends MenuActivity {
 
@@ -8,6 +11,11 @@ public class DrawableBackgroundImageActivity extends MenuActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_drawable);
+        
+        // set the scalable background
+        Bitmap bgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.background_image);
+        View view = findViewById(R.id.drawable);
+        view.setBackgroundDrawable(new ScalableDrawable(bgBitmap));
     }
 	
 }
